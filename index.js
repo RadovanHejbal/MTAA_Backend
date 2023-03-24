542e50217046584a4480ec959592f7ef1f91900a
Jaroslav <xdrozdj@stuba.sk>
2023-03-24 18:14:22 +0100

    dorobenie testerskych endpointov + par novych

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const pool = new Client({
   host: /*process.env.DATABASE_HOST ||*/ '127.0.0.1',
   port: /*process.env.DATABASE_PORT ||*/ 5432,
   database: /*process.env.DATABASE_NAME ||*/ 'mtaa',
-  password: /*process.env.DATABASE_PASSWORD ||*/ 'j4r0sl4v'
+  password: /*process.env.DATABASE_PASSWORD ||*/ ''
 });
 pool.connect();
 
