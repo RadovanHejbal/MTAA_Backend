f027db93e45fb42d9338a589d03ab4c7fc4edc49
Jaroslav <xdrozdj@stuba.sk>
2023-03-24 14:12:11 +0100

    login and regiter

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const { Client } = require("pg");
 
 const pool = new Client({
-  user: process.env.DATABASE_USER || 'postgres',
-  host: process.env.DATABASE_HOST || '127.0.0.1',
-  port: process.env.DATABASE_PORT || 5432,
-  database: process.env.DATABASE_NAME || 'mtaa',
-  password: process.env.DATABASE_PASSWORD || 'password'
+  user: /*process.env.DATABASE_USER ||*/ 'postgres',
+  host: /*process.env.DATABASE_HOST ||*/ '127.0.0.1',
+  port: /*process.env.DATABASE_PORT ||*/ 5432,
+  database: /*process.env.DATABASE_NAME ||*/ 'mtaa',
+  password: /*process.env.DATABASE_PASSWORD ||*/ ''
 });
 
 pool.connect();
 
 
-//EXAMPLE /*
-app.get('/login', (req, res) => {
-  pool.query(`SELECT * FROM users WHERE username = '${req.body.username}' AND password = '${req.body.password}'`, (err, response) => {
+// Login
+app.get('/login/:username/:password', (req, res) => {
+  pool.query(`
+          SELECT 
+              Users.id
+          FROM Users
+          WHERE Users.username = '${req.params.username}' AND Users.password = '${req.params.password}'
+          ;`, (err, response) => {
     if(err) {
       res.send(err);
       return;
     }
     res.send(response?.rows[0]);
   })
+})
+
+// Registration
+app.get('/registration/:username/:password/:email/:height/:weight/:gender/:role/:age/:firstname/:lastname/:dailykcal', (req, res) => {
+  pool.query(`
+        INSERT INTO Users ("id", username, "password", email, height, weight, gender, "role", age, firstname, lastname, dailykcal) 
+        VALUE
+            (uuid_in(md5(random()::text || random()::text)::cstring), '${req.paarams.username}', '${req.params.password}', '${req.params.email}', ${req.params.height}, ${req.params.weight}, '${req.params.gender}', '${req.params.role}', ${req.params.age}, '${req.params.firstname}', '${req.params.lastname}', ${req.params.dailykcal})
+        ;`, (err, response) => {
+    if(err){
+      res.send(err);
+      return;
+    }
+    res.send("Succesfully regitered");
+  }
+
+  )
 })
 
 app.get('/meals/:minimumcalorie', (req, res) => {
@@ -33,6 +56,7 @@ app.get('/meals/:minimumcalorie', (req, res) => {
     res.send(response?.rows);
   })
 })
+
 
 // EXAMPLE */
 
