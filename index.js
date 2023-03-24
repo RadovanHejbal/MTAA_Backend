490d2c2bb7680b0ea3e359d3f4ab4928b94a4621
RadovanHejbal <hejbalradovan@gmail.com>
2023-03-24 12:27:55 +0100

    example

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ const pool = new Client({
 
 pool.connect();
 
+
+//EXAMPLE /*
+app.get('/login', (req, res) => {
+  pool.query(`SELECT * FROM users WHERE username = '${req.body.username}' AND password = '${req.body.password}'`, (err, response) => {
+    if(err) {
+      res.send(err);
+      return;
+    }
+    res.send(response?.rows[0]);
+  })
+})
+
+app.get('/meals/:minimumcalorie', (req, res) => {
+  pool.query(`SELECT * FROM meals WHERE ${req.params.minimumcalorie} < meals.kcal`, (err, response) => {
+    if(err) {
+      res.send(err);
+      return;
+    }
+    res.send(response?.rows);
+  })
+})
+
+// EXAMPLE */
+
 app.listen(8000, () => {
   console.log("Listening on port: 8000");
 });
