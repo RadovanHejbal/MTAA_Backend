95c6b5a2d567ad83993cbae5319d3e2fdeaf2fc4
RadovanHejbal <hejbalradovan@gmail.com>
2023-04-12 22:29:12 +0200

    updating users/daily

--- routes/users.js
+++ routes/users.js
@@ -44,56 +44,77 @@ router.put('/change-username', (req, res) => {
     })
   })
   
-  // Daily kcal, protain, carbs, fat, activities for calcul
-  router.get('/daily/:date/:userid', (req, res) =>
+  // Daily kcal, protain, carbs, fat from meals
+  router.get('/daily/meals/:date/:userid', (req, res) =>
   {
+    const date = new Date(req.params.date);
+    const formattedDate = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString();
     const daily_query = `
-            WITH calc_meals AS 
-            (
-                SELECT 
-                    (meals.kcal * owned_meals.grams) AS kcal, 
-                    (meals.protein * owned_meals.grams) AS protein,
-                    (meals.fat * owned_meals.grams) AS fat, 
-                    (meals.carbohydrates * owned_meals.grams) AS carbs, 
-                    meals.title,
-                    owned_meals.id, 
-                    owned_meals.grams, 
-                    owned_meals.owner_id
-                FROM owned_meals
-                JOIN meals ON (owned_meals.meal_id = meals.id)
-                WHERE owned_meals.owner_id = ${req.params.userid} AND owned_meals.date = ${req.params.date}
-            ), calc_activities AS
-            (
-                SELECT
-                (
-                    (activities.kcal * owned_activities.time_amount) AS kcal,
-                    activities.title,
-                    owned_activities.id,
-                    owned_activities.time_amount,
-                    owned_activities.owner_id
-                )
-                FROM owned_activities
-                JOIN activities ON activities.id = owned_activities.activity_id
-                WHERE owned_activities.owner_id = ${req.params.userid} AND owned_activities.date = ${req.params.date}
-            )
-            SELECT 
-                ARRAY_AGG(JSON_BUILD_OBJECT('id', calc_meals.id, 'title', calc_meals.title, 'kcal', calc_meals.kcal, 'grams', calc_meals.grams,
-                                            'protein', calc_meals.protein, 'fat', calc_meals.fat, 'carbs', calc_meals.carbs)), 
-                ARRAY_AGG(JSON_BUILD_OBJECT('id', calc_activities.id, 'title', calc_activities.title, 'kcal', calc_activities.kcal, 
-                                            'time_amount', calc_activities.time_amount),
-                SUM(calc_activities.kcal),
-                SUM(calc_meals.kcal),
-                SUM(calc_meals.protein), 
-                SUM(calc_meals.fat), 
-                SUM(calc_meals.carbs)
-            FROM calc_meals
-            JOIN calc_activities ON calc_activities.owner_id = calc_meals.owner_id
-            GROUP BY calc_meals.owner_id
-            ;`;
+    WITH calc_meals AS 
+    (
+        SELECT 
+            (meals.kcal * owned_meals.grams) AS kcal, 
+            (meals.protein * owned_meals.grams) AS protein,
+            (meals.fat * owned_meals.grams) AS fat, 
+            (meals.carbohydrates * owned_meals.grams) AS carbs, 
+            meals.title,
+            owned_meals.id, 
+            owned_meals.grams, 
+            owned_meals.owner_id
+        FROM owned_meals
+        JOIN meals ON (owned_meals.meal_id = meals.id)
+        WHERE owned_meals.owner_id = '${req.params.userid}' AND DATE_TRUNC('day'::text, owned_meals.date) = DATE_TRUNC('day'::text, CAST('${formattedDate}' AS DATE))
+    )
+    SELECT 
+        ARRAY_AGG(JSON_BUILD_OBJECT('id', calc_meals.id, 'title', calc_meals.title, 'kcal', calc_meals.kcal, 'grams', calc_meals.grams,
+                                    'protein', calc_meals.protein, 'fat', calc_meals.fat, 'carbs', calc_meals.carbs)) AS meals, 
+        SUM(calc_meals.kcal) as kcal,
+        SUM(calc_meals.protein) as protein, 
+        SUM(calc_meals.fat) as fat, 
+        SUM(calc_meals.carbs) as carbs
+    FROM calc_meals
+    GROUP BY calc_meals.owner_id
+    ;`;
     pool.query(daily_query, (err, response) =>
     {
-      if(err) res.send(err);
-      else res.send(response?.rows[0]);
+      if(err) res.status(404).json(err);
+      else {
+        res.send(response?.rows[0]);
+      };
+    });
+  });
+
+  // daily kcal from activities
+  router.get('/daily/activities/:date/:userid', (req, res) =>
+  {
+    const date = new Date(req.params.date);
+    const formattedDate = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString();
+    const daily_query = `
+    WITH calc_activities AS
+    (
+        SELECT
+            (activities.kcal * owned_activities.time_amount) AS act_kcal,
+            activities.title,
+            owned_activities.id,
+            owned_activities.time_amount,
+            owned_activities.owner_id
+        FROM owned_activities
+        JOIN activities ON activities.id = owned_activities.activity_id
+        WHERE owned_activities.owner_id = '${req.params.userid}' AND DATE_TRUNC('day'::text, owned_activities.date) = DATE_TRUNC('day'::text, CAST('${formattedDate}' AS DATE))
+    )
+    SELECT 
+        ARRAY_AGG(JSON_BUILD_OBJECT('id', calc_activities.id, 'title', calc_activities.title, 'kcal', calc_activities.act_kcal, 
+                                    'time_amount', calc_activities.time_amount)) as activities,
+        SUM(calc_activities.act_kcal) as kcal
+    FROM calc_activities
+    GROUP BY calc_activities.owner_id
+    ;`;
+    pool.query(daily_query, (err, response) =>
+    {
+      if(err) res.status(404).json(err);
+      else {
+        res.send(response?.rows[0]);
+      };
     });
   });
 
